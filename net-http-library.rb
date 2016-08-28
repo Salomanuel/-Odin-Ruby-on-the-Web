75879d6bee7c7c2d4d71148c5cdf02231dae1847
Salomanuel <salomanue.aux+code@gmail.com>
2016-08-28 17:03:25 +0200

    progressing trought the exercises

--- net-http-library.rb
+++ net-http-library.rb
@@ -4,3 +4,13 @@ Net::HTTP.start('www.rubyinside.com') do |http|
 	req = Net::HTTP::Get.new('/test.txt')
 	puts http.request(req).body
 end
+
+url = URI.parse('http://www.rubyinside.com/test.txt')
+Net::HTTP.start(url.host, url.port) do |http|
+	req = Net::HTTP::Get.new(url.path)
+	puts http.request(req).body
+end
+
+url = URI.parse('http://www.rubyinside.com/test.txt')
+response = Net::HTTP.get_response(url)
+puts response.body
