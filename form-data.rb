d2826a009738f3851b962200ea69d3577304ae6c
Salomanuel <salomanue.aux+code@gmail.com>
2016-08-29 11:01:59 +0200

    add form-data

---
+++ form-data.rb
@@ -0,0 +1,14 @@
+require 'net/http'
+
+url = URI.parse('http://www.rubyinside.com/test.cgi')
+
+response = Net::HTTP.post_form(url,{'name' => 'David', 'age' => '24' })
+puts response.body
+
+puts "*" * 40
+
+Net::HTTP.start(url.host, url.port) do |http|
+	req = Net::HTTP::Post.new(url.path)
+	req.set_form_data({ 'name' => 'David', 'age' => '24' })
+	puts http.request(req).body
+end
